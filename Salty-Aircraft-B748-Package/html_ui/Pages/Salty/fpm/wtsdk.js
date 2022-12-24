b7ca433a953ff4095a5d8b0effc2df79d7ce1ae1
TJCAviation <80174397+TJCAviation@users.noreply.github.com>
2022-12-24 18:31:55 +0000

    Update: Reverted to Salty LNAV, Fixed BBJ no screens issue, added Auto Fuel Pumps Option

--- Salty-Aircraft-B748-Package/html_ui/Pages/Salty/fpm/wtsdk.js
+++ Salty-Aircraft-B748-Package/html_ui/Pages/Salty/fpm/wtsdk.js
@@ -4205,9 +4205,43 @@
         }
     }
 
+    class AutoFuel {
+        constructor() {
+            this.ovrdPumps = [7, 8, 11, 12];
+            this.xfeedValves = [1, 4];
+        }
+        update(_dt) {
+            const enabled = WTDataStore.get("AUTO_FUEL", 0) >= 1;
+            if (!enabled)
+                return;
+            const leftOutboardQuantity = SimVar.GetSimVarValue("FUEL TANK LEFT AUX QUANTITY", "gallon") + SimVar.GetSimVarValue("FUEL TANK LEFT TIP QUANTITY", "gallon");
+            const leftInboardQuantity = SimVar.GetSimVarValue("FUEL TANK LEFT MAIN QUANTITY", "gallon");
+            const rightOutboardQuantity = SimVar.GetSimVarValue("FUEL TANK RIGHT AUX QUANTITY", "gallon") + SimVar.GetSimVarValue("FUEL TANK RIGHT TIP QUANTITY", "gallon");
+            const rightInboardQuantity = SimVar.GetSimVarValue("FUEL TANK RIGHT MAIN QUANTITY", "gallon");
+            const tanksBalanced = leftInboardQuantity <= leftOutboardQuantity || rightInboardQuantity <= rightOutboardQuantity;
+            this.closePumpsAfterEmpty([15, 16], "FUEL TANK CENTER2 QUANTITY");
+            this.closePumpsAfterEmpty([1, 2], "FUEL TANK CENTER QUANTITY");
+            // Switch off OVRD/JETTISON pumps 2 & 3 and close crossfeed valves 1 & 4 when outboard + reserve tanks and inboard tanks balanced
+            this.ovrdPumps.forEach((pump) => SimVar.SetSimVarValue(`K:FUELSYSTEM_PUMP_${tanksBalanced ? "OFF" : "ON"}`, "number", pump));
+            this.xfeedValves.forEach((valve) => SimVar.SetSimVarValue(`K:FUELSYSTEM_VALVE_${tanksBalanced ? "CLOSE" : "OPEN"}`, "number", valve));
+        }
+        closePumpsAfterEmpty(pumps, quantityVar) {
+            const quantity = SimVar.GetSimVarValue(quantityVar, "Gallons");
+            for (const pump of pumps) {
+                const pumpActive = SimVar.GetSimVarValue(`FUELSYSTEM PUMP SWITCH:${pump}`, "bool");
+                if (quantity > 0 && !pumpActive) {
+                    SimVar.SetSimVarValue("K:FUELSYSTEM_PUMP_ON", "number", pump);
+                }
+                else if (quantity === 0 && pumpActive) {
+                    SimVar.SetSimVarValue("K:FUELSYSTEM_PUMP_OFF", "number", pump);
+                }
+            }
+        }
+    }
+
     class SaltyModules {
         constructor() {
-            this.modules = [new SoundManager(), new PauseAtTD()];
+            this.modules = [new SoundManager(), new PauseAtTD(), new AutoFuel()];
         }
         update(dt) {
             for (const module of this.modules) {
